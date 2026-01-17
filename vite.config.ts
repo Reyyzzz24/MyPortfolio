71e9746b516915df59962de4461b76fd273e0da0
Reva Yulian Satria <revayuliansatria@gmail.com>
2026-01-17 20:47:27 +0700

    ubah vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc' // atau @vitejs/plugin-react sesuai instalasi kamu
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Tambahkan baris ini. Pastikan namanya SAMA PERSIS dengan nama repository di GitHub
+  base: '/MyPortfolio/', 
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
