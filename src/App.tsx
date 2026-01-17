afd40dde34bb1320ded9db324273b5784b7d218f
Reyyzzz24 <revayuliansatria@gmai.com>
2026-01-17 20:50:52 +0700

    Update package.json for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Footer from "./sections/Footer";
 import DesignPhotography from "./pages/DesignPhotography"; // Pastikan path file benar
 import VideoEditing from "./pages/VideoEditing"; // Pastikan path file benar
 
+
 // Kita bungkus konten utama ke dalam satu komponen agar rapi
 const HomePage = () => (
   <>
