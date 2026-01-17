0d5e89c8217e622c6f17d2fa6061b76751bafc38
Reyyzzz24 <revayuliansatria@gmai.com>
2026-01-17 20:54:13 +0700

    Fix: change script src to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // atau @vitejs/plugin-react sesuai instalasi kamu
+import react from '@vitejs/plugin-react-swc' // Pastikan ini '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Tambahkan baris ini. Pastikan namanya SAMA PERSIS dengan nama repository di GitHub
   base: '/MyPortfolio/', 
   plugins: [
     react(),
